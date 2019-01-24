dedf9c8b32253cf4510e2382a31d155e04661e50
Abeyrathna <Chathuranga.Abeyrathna@pearson.com>
2019-01-24 16:42:48 +0530

    change

--- deploy/specs/php_spec.rb
+++ deploy/specs/php_spec.rb
@@ -6,7 +6,7 @@ describe_recipe 'deploy::php' do
 
   it 'creates a deployment directory' do
     node[:deploy].each do |application, deploy|
-      if deploy[:application_type] == 'php'
+      if deploy[:application_type] == 'other'
         directory(deploy[:deploy_to]).must_exist.with(:mode, '0775').and(:owner, deploy[:user]).and(:group, deploy[:group])
       end
     end
@@ -15,7 +15,7 @@ describe_recipe 'deploy::php' do
   ['log','config','system','pids','scripts','sockets'].each do |dir_name|
     it "creates a directory #{dir_name} in the deployment directory" do
       node[:deploy].each do |application, deploy|
-        if deploy[:application_type] == 'php'
+        if deploy[:application_type] == 'other'
           directory(::File.join(deploy[:deploy_to], 'shared', dir_name)).must_exist.with(:mode, '0770').and(:owner, deploy[:user]).and(:group, deploy[:group])
         end
       end
@@ -24,7 +24,7 @@ describe_recipe 'deploy::php' do
 
   it 'creates the apache site-available link' do
     node[:deploy].each do |application, deploy|
-      if deploy[:application_type] == 'php'
+      if deploy[:application_type] == 'other'
         file("#{node[:apache][:dir]}/sites-available/#{application}.conf").must_exist
       end
     end
@@ -32,7 +32,7 @@ describe_recipe 'deploy::php' do
 
   it 'creates the apache site-enabled link' do
     node[:deploy].each do |application, deploy|
-      if deploy[:application_type] == 'php'
+      if deploy[:application_type] == 'other'
         file("#{node[:apache][:dir]}/sites-enabled/#{application}.conf").must_exist
       end
     end
@@ -40,7 +40,7 @@ describe_recipe 'deploy::php' do
 
   it 'creates a logrotate configuration' do
     node[:deploy].each do |application, deploy|
-      if deploy[:application_type] == 'php'
+      if deploy[:application_type] == 'other'
         file("/etc/logrotate.d/opsworks_app_#{application}").must_exist.with(:mode, '0644').and(:owner, 'root').and(:group, 'root')
       end
     end
