2762a25a6ae7bafafef2ae2840a6bf0ba1282a53
Abeyrathna <Chathuranga.Abeyrathna@pearson.com>
2019-01-24 17:51:45 +0530

    change

--- app_deploy/recipes/php.rb
+++ app_deploy/recipes/php.rb
@@ -16,11 +16,21 @@ directory "#{doc_root}" do
   recursive true
 end
 
-s3_file "/tmp/#{file_name}" do
+s3_file "#{doc_root}/#{file_name}" do
     remote_path "/#{environment}/#{file_name}"
     bucket "#{s3_bucket_name}"
     owner "apache"
     group "apache"
     mode "0644"
     action :create
+end
+
+execute 'extract_package' do
+  command "tar -xf #{file_name}"
+  cwd "#{doc_root}"
+end
+
+file "#{doc_root}/#{file_name}" do
+  action :delete
+  only_if { File.exist? "#{doc_root}/#{file_name}" }
 end
