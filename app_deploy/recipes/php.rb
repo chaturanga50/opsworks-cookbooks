840f9efb5d3aa6b1c4c0126cd7ef73cf80b214d6
Abeyrathna <Chathuranga.Abeyrathna@pearson.com>
2019-01-24 19:31:49 +0530

    change

--- app_deploy/recipes/php.rb
+++ app_deploy/recipes/php.rb
@@ -3,14 +3,24 @@
 # Recipe:: php
 #
 
+time = Time.now.strftime("%F-%T").gsub(':','')
 app = search("aws_opsworks_app").first
+
+directory "#{app['attributes']['document_root']}" do
+  owner 'apache'
+  group 'apache'
+  mode '0644'
+  recursive true
+  action :create
+end
 
 template '/tmp/get_s3.sh' do
   source "get_s3.sh.erb"
   mode   "0755"
   variables({
     :url     => "#{app['app_source']['url']}",
-    :document_root     => "#{app['attributes']['document_root']}" 
+    :document_root     => "#{app['attributes']['document_root']}",
+    :short_name => "#{app['shortname']}"
   })
   owner 'apache'
   group 'apache'
